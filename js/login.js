848d693fd1fedcc4b419d7f66100298359f60398
Marcos Henrique <henriquemarcos747@gmail.com>
2023-10-01 19:56:33 -0300

    jhml,jmh

--- js/login.js
+++ js/login.js
@@ -1,4 +1,28 @@
 function acessar() {
-    window.location.href = 'public/pagina_inicial.html';
+  window.location.href = 'public/pagina_inicial.html';
+}
+
+
+// Seleciona o elemento <select>
+const selectElement = document.getElementById('entrarComo');
+
+// Seleciona o elemento <span> que deve ser mostrado/ocultado
+const emailSpan = document.getElementById('emailAdequando');
+
+// Adicione um ouvinte de evento ao elemento <select>
+selectElement.addEventListener('change', function modoDeEntrada() {
+  // Verifica o valor selecionado
+  if (selectElement.value === 'aluno') {
+    emailSpan.innerHTML = "@al.educacao.sp.gov.br";
   }
 
+  if (selectElement.value === 'professor') {
+    emailSpan.innerHTML = "@professor.educacao.sp.gov.br";
+  }
+
+  if (selectElement.value === 'dev') {
+    emailSpan.innerHTML = "";
+  }
+});
+
+modoDeEntrada();

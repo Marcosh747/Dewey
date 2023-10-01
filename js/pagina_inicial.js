199c0023111b76a8932d682cdd17b9bf461581c1
Marcos Henrique <henriquemarcos747@gmail.com>
2023-10-01 18:51:40 -0300

    jkhbj

--- js/pagina_inicial.js
+++ js/pagina_inicial.js
@@ -212,15 +212,15 @@ document.addEventListener('DOMContentLoaded', function () {
         let primeiro_dia = new Date(ano, mes, 1);
 
         for (let i = 0; i <= dias_do_mes[mes] + primeiro_dia.getDay() - 1; i++) {
-            let dia = document.createElement('div');
+            let dia = document.createElement('button');
             if (i >= primeiro_dia.getDay()) {
-                dia.classList.add('dia-do-calendario-hover', 'semana-5');
+                dia.classList.add('botoes');
                 let numeroDia = i - primeiro_dia.getDay() + 1;
                 dia.innerHTML = `
                     <div class="container2">
-                        <div class="camada-estado3">
-                            <div class="data">${numeroDia}</div>
-                        </div>
+                        
+                        <span class="data">${numeroDia}</span>
+                       
                     </div>
                 `;
                 let dataAtual = new Date();

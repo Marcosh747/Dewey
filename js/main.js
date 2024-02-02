c35239ae23b760a7984c1ac55dd53cda7399420b
Marcos Henrique <henriquemarcos747@gmail.com>
2024-02-02 15:11:42 -0300

    modified:   css/main.css 	modified:   css/pagina_inicial.css 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -165,36 +165,59 @@ function createPopup(title, content) {
 
 
 // TESTE
+var anguloAtual = 0; // Variável global para armazenar o ângulo atual
+
 function exibirImagem(src) {
-  // Cria a div de fundo transparente dinamicamente
   var fundoTransparente = document.createElement('div');
   fundoTransparente.className = 'fundoTransparente';
   fundoTransparente.id = 'fundoTransparente_imagemAmpliada';
 
-  // Cria a imagem dentro da div
   var imagemAmpliada = document.createElement('img');
   imagemAmpliada.id = 'imagemAmpliada';
   imagemAmpliada.alt = 'Imagem Ampliada';
   imagemAmpliada.src = src;
 
-  // Adiciona a imagem à div
   fundoTransparente.appendChild(imagemAmpliada);
 
-  // Adiciona a div ao corpo do documento
   document.body.appendChild(fundoTransparente);
 
-  // Exibe a div de fundo transparente
   fundoTransparente.style.display = 'flex';
 
-  // Verifica se a largura da imagem é maior que a largura da tela
   if (imagemAmpliada.width > window.innerWidth) {
-    // Aplica rotação se necessário
-    imagemAmpliada.style.transform = 'rotate(90deg)';
+    imagemAmpliada.style.transform = 'rotate(' + anguloAtual + 'deg)';
   }
 
-  // Adiciona um evento de clique para fechar a imagem
-  fundoTransparente.addEventListener('click', function() {
-    // Remove a div ao ser fechada
+  var btnGirar = document.createElement('button');
+  btnGirar.id = 'btnGirar';
+  btnGirar.className = 'botao-selecionado botoes';
+
+  btnGirar.addEventListener('click', function (e) {
+    e.stopPropagation();
+    girarImagem();
+  });
+
+  var iconeSvg = '<svg fill="#161E0A" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">' +
+    '<path d="M393-132q-103-29-168-113.5T160-440q0-57 19-108.5t54-94.5q11-12 27-12.5t29 12.5q11 11 11.5 27T290-586q-24 31-37 68t-13 78q0 81 47.5 144.5T410-209q13 4 21.5 15t8.5 24q0 20-14 31.5t-33 6.5Zm174 0q-19 5-33-7t-14-32q0-12 8.5-23t21.5-15q75-24 122.5-87T720-440q0-100-70-170t-170-70h-3l16 16q11 11 11 28t-11 28q-11 11-28 11t-28-11l-84-84q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l84-84q11-11 28-11t28 11q11 11 11 28t-11 28l-16 16h3q134 0 227 93t93 227q0 109-65 194T567-132Z"></path>' +
+    '</svg>';
+
+  btnGirar.innerHTML = iconeSvg;
+  fundoTransparente.appendChild(btnGirar);
+
+  fundoTransparente.addEventListener('click', function () {
     document.body.removeChild(this);
   });
+}
+
+function girarImagem() {
+  var imagemAmpliada = document.getElementById('imagemAmpliada');
+
+  // Incrementa o ângulo em 90 graus
+  anguloAtual += 90;
+
+  // Verifica se o ângulo atual é múltiplo de 180 (volta ao original)
+  if (anguloAtual % 180 === 0) {
+    anguloAtual = 0;
+  }
+
+  imagemAmpliada.style.transform = 'rotate(' + anguloAtual + 'deg)';
 }
